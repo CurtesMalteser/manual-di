4ec2818b8cf2ec09f791dac8ebffd7049d1dd7c8
Curtes Malteser <antonio_bastiao@hotmail.com>
2021-11-10 21:45:09 +0100

    initial commit

---
+++ app/src/main/java/com/curtesmalteser/manualdi/App.kt
@@ -0,0 +1,10 @@
+package com.curtesmalteser.manualdi
+
+import android.app.Application
+
+/**
+ * Created by António Bastião on 10.11.21
+ * Refer to <a href="https://github.com/CurtesMalteser">CurtesMalteser github</a>
+ */
+class App: Application() {
+}
